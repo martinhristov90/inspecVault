74f0b4ed8920c31d95b36678117c81b5f7ba74d9
Martin Hristov <martinkrhristov@gmail.com>
2019-07-12 07:43:26 +0300

    updating controls

--- controls/vault_test.rb
+++ controls/vault_test.rb
@@ -3,8 +3,12 @@ control 'vault_install' do
     title 'vault_install'
     desc 'Checks if vault is installed'
     
-    describe service('vault') do
+    describe systemd_service('vault') do
         it { should be_enabled }
         it { should be_running }
+    end
+    describe port(8200) do
+        it { should be_listening }
+        its('addresses') {should include '0.0.0.0'}
       end
 end
