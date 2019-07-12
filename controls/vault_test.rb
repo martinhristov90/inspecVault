1038a85a3b93d2031c63952b0ae4d2f643270710
Martin Hristov <martinkrhristov@gmail.com>
2019-07-12 09:58:24 +0300

    doing some formatting

--- controls/vault_test.rb
+++ controls/vault_test.rb
@@ -1,14 +1,14 @@
 control 'vault_install' do
-
-    title 'vault_install'
+    title 'vault_install_check'
     desc 'Checks if vault is installed'
     
     describe systemd_service('vault') do
         it { should be_enabled }
         it { should be_running }
     end
+
     describe port(8200) do
         it { should be_listening }
         its('addresses') {should include '0.0.0.0'}
-      end
+    end
 end
